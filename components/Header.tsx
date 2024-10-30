89abc70d824dee8381eec297805568f432b6ca99
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-10-30 08:51:22 +0530

    hi

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/nextjs"
+
+function Header() {
+  const {user} = useUser()
+  return (
+    <div>
+      {user && (
+        <h1>{user?.firstName}
+        {`'s`} Space
+        </h1>
+      )}
+
+      {/* Breadcrumbs */}
+            <div>
+              <SignedOut>
+                  <SignInButton/>
+              </SignedOut>
+              
+              <SignedIn>
+                  <UserButton/>
+              </SignedIn>
+            </div>
+    </div>
+  )
+}
+
+export default Header
